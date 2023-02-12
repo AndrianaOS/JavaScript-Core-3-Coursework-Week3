00b88bead0f848b818bc6d3deacf673ea32d8df7
Andriana <andrianasaffo@gmail.com>
2023-02-12 14:41:45 +0000

    exercise 3 completed

--- exercises/exercise-3/exercise-3.js
+++ exercises/exercise-3/exercise-3.js
@@ -6,3 +6,12 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
   { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
 ];
+
+let total = 0;
+
+for (let { itemName: food, quantity: num, unitPrice: price } of order) {
+  console.log(`${num} ${food} ${price * num}`);
+
+  total = total + price * num;
+}
+console.log(`Total: ${total}`);
